40a5dc8ac66cb7baa2c36ac4c27efe695a23027a
klouxia <klouxia@gmail.com>
2023-02-14 15:31:28 +0900

    initial

---
+++ src/main/java/com/example/todo/controller/helloController.java
@@ -0,0 +1,13 @@
+package com.example.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To do Application";
+    }
+}
